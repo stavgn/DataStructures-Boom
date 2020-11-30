c030bef9009ed8e366d03a1058d68f451b16bab7
DorShacham <dor.shacham@gmail.com>
2020-11-30 10:22:17 +0200

    tree+node.h was updated

--- tree_node.h
+++ tree_node.h
@@ -7,12 +7,13 @@ namespace DS
     class tree_node
     {
     private:
-        const int key_array_size = 3;
+        const int index_array_size = 3;
         /* data */
     public:
-        KEY key[3];
-        tree_node<KEY, DATA> *children_array[4];
-        DATA *data_ptr;
+        KEY key;                                 //in case of leaf
+        KEY index_array[3];                      //in case of node
+        tree_node<KEY, DATA> *children_array[4]; //in case of node
+        DATA *data_ptr;                          //in case of leaf
         int length;
 
         tree_node(KEY key = KEY());
@@ -27,8 +28,9 @@ namespace DS
                                                                nullptr}),
                                                data_ptr(nullptr)
     {
-        key[0](key);
-        length(1);
+        key(key);
+        index_array[0](key);
+        length(0);
     }
     template <class KEY, class DATA>
     tree_node<KEY, DATA>::~tree_node()
@@ -38,21 +40,25 @@ namespace DS
     template <class KEY, class DATA>
     void tree_node<KEY, DATA>::insert(tree_node *node)
     {
-        for (int place_in_node = 0; place_in_node < length - 1; place_in_node++)
+        int place_in_node;
+        for (int place_in_node = 0; place_in_node < length; place_in_node++)
         {
-            if (node->key[0] < key[place_in_node])
+            if (node->key < children_array[place_in_node]->key)
             {
-                for (int i = length - 1; i > place_in_node; i--)
+                for (int i = length; i > place_in_node; i--)
                 {
                     children_array[i] = children_array[i - 1];
-                    if (i < key_array_size)
+                    if (i > place_in_node)
                     {
-                        key[i] = key[i - 1];
+                        index_array[i - 1] = children_array[i]->key[0];
                     }
                 }
-                children_array[place_in_node] = node;
-                key[place_in_node] = children_array[place_in_node + 1]->key[0];
             }
+        }
+        children_array[place_in_node] = node;
+        if (place_in_node > 0)
+        {
+            index_array[place_in_node - 1] = node->key;
         }
     }
 
