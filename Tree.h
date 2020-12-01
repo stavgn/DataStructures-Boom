dfd90108a7121103ac0377e644a636c9fe98204c
DorShacham <dor.shacham@gmail.com>
2020-12-01 20:22:51 +0200

    remove_by_ptr almost finished

--- Tree.h
+++ Tree.h
@@ -14,6 +14,7 @@ namespace DS
         tree_node<KEY, DATA> *find_node(KEY key, tree_node<KEY, DATA> *father_node);
         bool is_leaf(tree_node<KEY, DATA> *node);
         bool insert_node_by_ptr(tree_node<KEY, DATA> *node, tree_node<KEY, DATA> *father_node);
+        void remove_node_by_ptr(tree_node<KEY, DATA> *node, tree_node<KEY, DATA> *father_node);
 
     public:
         Tree();
@@ -43,7 +44,10 @@ namespace DS
     template <class KEY, class DATA>
     tree_node<KEY, DATA> *Tree<KEY, DATA>::find_node(KEY key, tree_node<KEY, DATA> *father_node)
     {
-        assert(father_node != nullptr);
+        if (father_node == nullptr)
+        {
+            return nullptr;
+        }
         if (is_leaf(father_node))
         {
             if (father_node->key == key)
@@ -197,6 +201,78 @@ namespace DS
         }
         return true;
     }
+    template <class KEY, class DATA>
+    void Tree<KEY, DATA>::remove_node_by_ptr(tree_node<KEY, DATA> *node, tree_node<KEY, DATA> *father_node)
+    {
+        assert(node != nullptr);
+        if (find_node(node, root_ptr) == nullptr)
+        {
+            return;
+        }
+        if ((father_node == root_ptr) && (is_leaf(root_ptr)))
+        {
+            assert(root_ptr == node);
+            delete node->data_ptr;
+            delete node;
+            root_ptr = max_leaf = nullptr;
+            return;
+        }
+        if (is_leaf(father_node->children_array[0]))
+        {
+            tree_node<KEY, DATA> *removen_node = father_node->remove(node->key);
+            assert(removen_node != nullptr);
+            if (node->left_ptr == nullptr)
+            {
+                assert(node->right_ptr != nullptr);
+                node->right_ptr->left_ptr = nullptr;
+            }
+            else if (node->right_ptr == nullptr) //That means node is the bigest leaf
+            {
+                assert(node->leaft_ptr != nullptr);
+                assert(node = max_leaf);
+                node->leaft_ptr->right_ptr = nullptr;
+            }
+            else
+            {
+                node->left_ptr->right_ptr = node->right_ptr;
+                node->right_ptr->left_ptr = node->left_ptr;
+            }
+            delete data;
+            delete node;
+            return;
+        }
+        if (!is_leaf(father_node->children_array[0]))
+        {
+            int i;
+            for (i = 0; ((i < father_node->length - 1) && !(node->key < father_node->index_array[i])); i++)
+                ;
+            assert(father_node->children_array[i] != nullptr);
+            tree_node<KEY, DATA> *child_node = father_node->children_array[i];
+            remove_node_by_ptr(node, child_node);
+            if (child_node->length < 2)
+            {
+                assert(child_node->length == 1);
+                /*******
+                 * 
+                 * 
+                 * 
+                 * 
+                 * need to fill this part
+                 * 
+                 * 
+                 * ***/
+            }
+        }
+        if (root_ptr->length < 2)
+        {
+            assert(root_ptr->length == 1);
+            assert(!is_leaf(root_ptr));
+            tree_node<KEY, DATA> *child_node = root_ptr->children_array[0];
+            delete root_ptr;
+            root_ptr = child_node;
+        }
+    }
+
 } // namespace DS
 
 #endif
